6412fdb9f2ae6cb7c0059ba536267527c9b771ac
seigenji <admin@pc-b003.local>
2017-04-27 13:42:41 +0900

    二項演算子は上手くいかないので削除

--- cointoss.R
+++ cointoss.R
@@ -11,12 +11,6 @@ result = coin_toss()
 coin_mean = mean(result)
 squared_sum = .0
 
-"%+=%" <- function(x, y)x <<- x + y  # C 風のインクリメント演算子
-x <- 10
-x %+=% 5
-x
-a = 3
-a %+=% 5
 for(i in 1:5){
   squared_sum = squared_sum + (result[i] - coin_mean)^2
 }
